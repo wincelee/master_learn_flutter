80e2ae6291970da7fd2073f9894417ad93fd7ad1
AguerWinceville <aguerwinceville@gmail.com>
2022-01-31 12:08:50 +0300

    12:08 31st January 2022 from Hp Pc

--- lib/classes/config.dart
+++ lib/classes/config.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 class Config {
   static const String nullNetworkImage =
       "https://d1csarkz8obe9u.cloudfront.net/"
@@ -6,6 +8,10 @@ class Config {
 
   static const String nullIndexHero = "null_index_hero";
   static const int responseTimeOutInSeconds = 10;
+
+  static const Color primaryColor = Color(0xff7e1416);
+  static const Color primaryDarkColor = Color(0xff4d0000);
+  static const Color accentColor = Color(0xffF9A61A);
 
   bool equalsIgnoreCase(String string1, String string2) {
     return string1.toLowerCase() == string2.toLowerCase();
--- lib/login_screens/login_one.dart
+++ lib/login_screens/login_one.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:master_learn/classes/config.dart';
 
 class LoginOne extends StatefulWidget {
   const LoginOne({Key? key}) : super(key: key);
@@ -29,17 +30,27 @@ class _LoginOneState extends State<LoginOne> {
           Padding(
               padding: const EdgeInsets.all(10),
               child: TextFormField(
-                  controller: phoneNoController,
-                  decoration:
-                      const InputDecoration(hintText: "Enter Phone Number"),
-                  // Numbers Allow Decimals
-                  keyboardType: TextInputType.number,
-                  )),
+                controller: phoneNoController,
+                decoration: const InputDecoration(
+                    // Input decoration with both hintText and labelText
+                    hintText: "Enter Phone Number",
+                    labelText: "Enter Phone Number"),
+                // Numbers Allow Decimals
+                keyboardType: TextInputType.number,
+              )),
           Padding(
               padding: const EdgeInsets.all(10),
               child: TextFormField(
                 controller: pinController,
-                decoration: const InputDecoration(hintText: "Enter Pin"),
+                decoration: const InputDecoration(
+                    // Input decoration with labelText only
+                    labelText: "Enter Pin",
+                    // Start Icon
+                    prefixIcon: Icon(Icons.lock_outline_rounded),
+                    prefixIconColor: Color(0xff7e1416),
+                    // End Icon
+                    suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                    suffixIconColor: Config.primaryColor),
                 // Numbers Keyboard
                 keyboardType: TextInputType.number,
                 // Numbers Only No Decimal
@@ -48,7 +59,27 @@ class _LoginOneState extends State<LoginOne> {
                   // Or user below code
                   // FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
                 ],
-              ))
+              )),
+          Padding(
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                child: Text("Log In".toUpperCase(),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    )),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    // Button Background Color
+                    primary: Config.primaryColor,
+                    // Ripple Color
+                    onPrimary: Config.accentColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    // Set Elevated Button Width and Height Respectively 200 and 50 using Size
+                    minimumSize: const Size(300, 40),
+                    maximumSize: const Size(300, 40)),
+              )),
         ],
       ),
     );
