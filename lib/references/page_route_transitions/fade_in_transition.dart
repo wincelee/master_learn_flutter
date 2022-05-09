0ac47b31033023aaaeb2918f874f3d2dffd321df
emmanjorodongo <emmanjorodongo@gmail.com>
2022-05-09 12:55:34 +0300

    Page Route Transitions

---
+++ lib/references/page_route_transitions/fade_in_transition.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class FadeInTransition extends PageRouteBuilder {
+  final Widget page;
+
+  FadeInTransition({required this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+        );
+}
